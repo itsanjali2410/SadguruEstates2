de649e6700fdeb433f6d6d895308e6c3ac086059
Anjali <itsanjali2410@gmail.com>
2025-06-07 18:37:38 +0530

    updated files

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { defineConfig, loadEnv } from 'vite';
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, '.', '');
   return {
-    base: '/SadguruEstates/', // Add this line for GitHub Pages deployment
+    base: '/SadguruEstates2/', // Add this line for GitHub Pages deployment
     define: {
       'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
       'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
